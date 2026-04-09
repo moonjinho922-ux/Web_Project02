eaa5bab8f5e7de6de6aa434e97dcd22d39c25ff9
Trinh Bui <trinh.buii04@gmail.com>
2026-04-09 10:42:39 -0400

    changes to php

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 // testing phase - hard code
 $users = [
-    "player1" => password_hash("123", PASSWORD_DEFAULT),
-    "player2" => password_hash("456", PASSWORD_DEFAULT),
+    "test1" => password_hash("123", PASSWORD_DEFAULT),
+    "test2" => password_hash("456", PASSWORD_DEFAULT),
 ];
 
 function getUserByUsername($username) {
